a1cdde325e688d84ee3cce9cf2f8261bec8c685e
Lynzle Baldelomar <lynzle.baldelomar@infor.com>
2020-12-04 11:39:47 +0800

    fix

--- src/app/idm-modal/idm-modal.component.ts
+++ src/app/idm-modal/idm-modal.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./idm-modal.component.scss']
 })
 export class IdmModalComponent implements OnInit {
+  images: string[];
 
   constructor() { }
 
